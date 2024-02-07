f1d9fc1f815f500082c55d085b99a9b6c9bd9436
seonghokim <kims9872@naver.com>
2024-02-08 01:18:23 +0900

    20240208_Summarize C++

--- practice_cpp.cpp
+++ practice_cpp.cpp
@@ -1,17 +1,26 @@
-
-
-#include <iostream>
-using namespace std;
-void fun(int);
-void fun(int*);
+int* p = new int(25);
+float* q = new float(75.25);
+ 
+// Custom data type
+struct cust
+{
+    int p;
+    cust(int q) : p(q) {}
+    cust() = default;
+    //cust& operator=(const cust& that) = default;
+};
+ 
 int main()
 {
-    int a = 10;
-    fun(&a); // error, which fun()?
+    // Works fine, doesn’t require constructor
+    cust* var1 = new cust;
+ 
+    //OR
+ 
+    // Works fine, doesn’t require constructor
+    var1 = new cust();
+ 
+    // Notice error if you comment this line
+    cust* var = new cust(25);
     return 0;
-}
-void fun(int x) { cout << "Value of x is : " << x << endl; }
-void fun(int* b)
-{
-    cout << "Value of b is : " << b << endl;
 }
