ce076124196b80735958ad22156525dac68b0c2e
seonghokim <kims9872@naver.com>
2024-02-09 01:42:26 +0900

    20240209_Summarize C++

--- practice_cpp.cpp
+++ practice_cpp.cpp
@@ -1,26 +1,21 @@
-int* p = new int(25);
-float* q = new float(75.25);
- 
-// Custom data type
-struct cust
-{
-    int p;
-    cust(int q) : p(q) {}
-    cust() = default;
-    //cust& operator=(const cust& that) = default;
-};
- 
-int main()
-{
-    // Works fine, doesn’t require constructor
-    cust* var1 = new cust;
- 
-    //OR
- 
-    // Works fine, doesn’t require constructor
-    var1 = new cust();
- 
-    // Notice error if you comment this line
-    cust* var = new cust(25);
-    return 0;
-}
+#include<iostream> 
+using namespace std; 
+  
+class Test 
+{ 
+private: 
+  int x; 
+public: 
+  Test(int x = 0) { this->x = x; } 
+  void change(Test *t) { this = t; } 
+  void print() { cout << "x = " << x << endl; } 
+}; 
+  
+int main() 
+{ 
+  Test obj(5); 
+  Test *ptr = new Test (10); 
+  obj.change(ptr); 
+  obj.print(); 
+  return 0; 
+} 
