9e9cd5a6b93eecbd37b7449bfd0354c2e3e87644
seonghokim <kims9872@naver.com>
2024-02-12 23:47:17 +0900

    20240212_Summarize C++

--- practice_cpp.cpp
+++ practice_cpp.cpp
@@ -37,7 +37,7 @@ int main()
     t.print();
     return 0;
 } */
-#include <iostream>
+/* #include <iostream>
 using namespace std;
  
 class overload {
@@ -81,4 +81,37 @@ int main()
             "  ";
     i.Display();
     return 0;
+} */
+
+
+#include <iostream> 
+  
+using namespace std; 
+  
+template <class T> class Test 
+{   
+private: 
+  T val;  
+public: 
+  static int count; 
+  Test() 
+  { 
+    count++; 
+  } 
+  // some other stuff in class 
+}; 
+  
+template<class T> 
+int Test<T>::count = 0; 
+  
+int main() 
+{ 
+  Test<int> a, b;
+  //Test<int> b;  // value of count for Test<int> is 1 now 
+  Test<double> c;  // value of count for Test<double> is 1 now 
+  cout << Test<int>::count   << endl;  // prints 2   
+  cout << Test<double>::count << endl; //prints 1 
+     
+  //getchar(); 
+  return 0; 
 }
