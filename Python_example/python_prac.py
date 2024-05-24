1ca0162df41e9552d0bd5dc425db92c13cd2a533
seonghokim <kims9872@naver.com>
2024-05-25 01:05:02 +0900

    20240524_Recap

--- Python_example/python_prac.py
+++ Python_example/python_prac.py
@@ -1,16 +1,3 @@
-dict1 = {10: 'a', 20: [1, 2, 3], 30: 'c'}
-print("Given Dictionary:", dict1)
- 
-# new dictionary and
-# copying using copy() method
-dict2 = dict1.copy()
-print("New copy:", dict2)
- 
-# Updating dict2 elements and
-# checking the change in dict1
-dict2[10] = 10
-dict2[20][2] = '45'  # list item updated
- 
-print("Updated copy:", dict2)
-
-print(dict1)
+str = "Geeksforgeeks"
+print("".join(list(reversed(str))))
+print(str)
