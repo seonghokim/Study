a2a61342af58ff34d153f6cfdf0169712da48875
seonghokim <kims9872@naver.com>
2024-05-25 23:57:24 +0900

    20240525_Recap

--- Python_example/python_prac.py
+++ Python_example/python_prac.py
@@ -1,3 +1,13 @@
-str = "Geeksforgeeks"
-print("".join(list(reversed(str))))
-print(str)
+
+import itertools
+import operator
+
+GFG = [5, 3, 6, 2, 1, 9, 1]
+
+result = itertools.accumulate(GFG, max)
+
+for each in result:
+    print(each)
+
+
+
