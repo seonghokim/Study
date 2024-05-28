99afc42e9c943a462e5fc7eda7a45dc06e5eec5c
seonghokim <kims9872@naver.com>
2024-05-28 23:51:00 +0900

    20240528_Recap

--- Python_example/python_prac.py
+++ Python_example/python_prac.py
@@ -1,9 +1,21 @@
-def simpleGeneratorFun():
-    yield 1
-    yield 2
-    yield 3
- 
- 
-# Driver code to check above generator function
-for value in simpleGeneratorFun():
-    print(value)
+def runMatch(mystr):
+    
+    # match case
+    match mystr:
+        # pattern 1
+        case ["a"]:
+            print("a")
+        # pattern 2
+        case ["a", *b]:
+            print(f"a and {b}")
+        # pattern 3
+        case (*a, "e"):
+            print(f"{a} and e")#(*a, "e"), [*a, "e"]
+        # default pattern
+        case _:
+            print("No data")
+            
+runMatch([])
+runMatch(["a"])
+runMatch(["a", "b"])
+runMatch(["b", "c", "d", "e"])
