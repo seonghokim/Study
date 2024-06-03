ee6de00996a12b06409897efc989f6384f484ffd
seonghokim <kims9872@naver.com>
2024-06-04 02:08:07 +0900

    20240603_Recap

--- Python_example/python_prac.py
+++ Python_example/python_prac.py
@@ -1,8 +1,22 @@
 
+class BirthDay:
+    def __init__(self, birth_year: int, birth_month: int, birth_date: int):
+        self.year = birth_year
+        self.month = birth_month
+        self.date = birth_date
 
-from re import split 
-  
-print(split('\W+', 'Words, words , Words')) 
-print(split('\W+', "Word's words Words")) 
-print(split('\W+', 'On 12th Jan 2016, at 11:02 AM')) 
-print(split('\d+', 'On 12th Jan 2016, at 11:02 AM')) 
+    @classmethod
+    def by_security_number(cls, security_number: str):
+        birth_year = 1900 + int(security_number[:2])
+        birth_month = int(security_number[2:4])
+        birth_date = int(security_number[4:6])
+        return cls(birth_year, birth_month, birth_date)
+
+
+if __name__ == "__main__":
+    birth1 = BirthDay(1999, 1, 31)
+    birth2 = BirthDay.by_security_number("990131")
+    print(birth1.year)
+    print(birth2.year)
+
+
